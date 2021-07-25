eef201f25c340317a69e382890f55e1b0eef1628
Vitalii <vithom76@gmail.com>
2021-07-25 20:18:40 +0300

    fitst

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//alert('Hello');
+//
+//let result = confirm("Are you here?");
+//console.log(result);
+
+//let answer = prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+
+//const answers = [];
+//answers[0] = prompt("Как ваше имя?", "");
+//answers[1] = prompt("Как ваша фамилия?", "");
+//answers[2] = prompt("Сколько вам лет?", "");
+
+//document.write(answers);
+//console.log(typeof (answers));
+const category = 'toys';
+
+console.log(`https://someurl.com/${ category } / 5`);
+
+console.log("arr" + "-object");
+console.log();
